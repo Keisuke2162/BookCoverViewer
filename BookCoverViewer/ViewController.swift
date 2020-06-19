507fb998bf97ee8b952d0695664a65fc59e47a42
Keisuke2162 <applabo.kei@gmail.com>
2020-06-20 07:40:13 +0900

    Initial Commit

---
+++ BookCoverViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookCoverViewer
+//
+//  Created by 植田圭祐 on 2020/06/20.
+//  Copyright © 2020 Keisuke Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
